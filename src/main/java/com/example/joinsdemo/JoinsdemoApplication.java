6ffdde94590003acebedd60544527826c586a30e
Skanda <skanda.td@gmail.com>
2025-05-05 11:11:08 +0530

    Spring boot starter

---
+++ src/main/java/com/example/joinsdemo/JoinsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.joinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinsdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/joinsdemo/JoinsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
